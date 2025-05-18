fa7b819117f1da4db594d1becb9d10ef391fcb96
davirg12 <riveran@utb.edu.co>
2025-05-18 18:27:52 -0500

    Update test_order_not_existent_id.py

--- shipping_order_service/test/negative_integration_tests/test_order_not_existent_id.py
+++ shipping_order_service/test/negative_integration_tests/test_order_not_existent_id.py
@@ -1,3 +1,7 @@
+import pytest
+from httpx import AsyncClient
+from app.main import app
+from httpx._transports.asgi import ASGITransport
 @pytest.mark.asyncio
 async def test_get_order_not_found():
     transport = ASGITransport(app=app)
