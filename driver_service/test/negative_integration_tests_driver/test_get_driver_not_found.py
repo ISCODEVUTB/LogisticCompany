8e7329077e12076ce0093cc3211f157342499e30
davirg12 <riveran@utb.edu.co>
2025-05-18 18:24:12 -0500

    Update test_get_driver_not_found.py

--- driver_service/test/negative_integration_tests_driver/test_get_driver_not_found.py
+++ driver_service/test/negative_integration_tests_driver/test_get_driver_not_found.py
@@ -1,5 +1,6 @@
 import pytest
-from httpx import AsyncClient, ASGITransport
+from httpx import AsyncClient
+from httpx._transports.asgi import ASGITransport
 from app.main import app
 
 @pytest.mark.asyncio
