8142df8a96fb52a12028e97f451e242f8afee808
davirg12 <riveran@utb.edu.co>
2025-05-18 18:19:53 -0500

    Update test_create_shipping_order.py

--- shipping_order_service/test/test_integration_shipping/test_create_shipping_order.py
+++ shipping_order_service/test/test_integration_shipping/test_create_shipping_order.py
@@ -1,6 +1,6 @@
 import pytest
 from httpx import AsyncClient, ASGITransport
-from app.main import 
+from app.main import app
 
 @pytest.mark.asyncio
 async def test_create_shipping_order_integration():
