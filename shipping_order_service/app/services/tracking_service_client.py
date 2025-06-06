54cbf917f97f523c422919539803d2344a7ce4b9
davirg12 <riveran@utb.edu.co>
2025-06-05 19:36:40 -0500

    Update tracking_service_client.py

--- shipping_order_service/app/services/tracking_service_client.py
+++ shipping_order_service/app/services/tracking_service_client.py
@@ -1,19 +1,15 @@
-import os
-from datetime import datetime
-
 import httpx
+from datetime import datetime
+import os
 
-TRACKING_SERVICE_URL = os.getenv(
-    "TRACKING_SERVICE_URL", "http://localhost:8003/tracking/track"
-)
-
+TRACKING_SERVICE_URL = os.getenv("TRACKING_SERVICE_URL", "http://localhost:8003/tracking/track")
 
 async def send_tracking_event(tracking_code: str, status: str, timestamp: datetime):
     url = TRACKING_SERVICE_URL
     payload = {
         "tracking_code": tracking_code,
         "status": status,
-        "timestamp": timestamp.isoformat(),
+        "timestamp": timestamp.isoformat()
     }
 
     async with httpx.AsyncClient() as client:
