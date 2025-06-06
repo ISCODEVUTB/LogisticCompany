5b7926a4504cf1ecf1cefa51922320431b856600
davirg12 <riveran@utb.edu.co>
2025-06-06 13:58:24 -0500

    Update test_delete_route_success.py

--- routing_service/test/test_integration_routing/test_delete_route_success.py
+++ routing_service/test/test_integration_routing/test_delete_route_success.py
@@ -1,6 +1,6 @@
 import pytest
 from httpx import AsyncClient, ASGITransport
-from routing_service.app.main import app
+from app.main import app
 import uuid
 
 @pytest.mark.asyncio
@@ -16,11 +16,11 @@ async def test_delete_route_success():
             "driver_id": None,
             "order_ids": []
         }
-        create_response = await client.post("/api/routes/", json=payload)
+        create_response = await client.post("/routes/", json=payload)
         assert create_response.status_code == 200 # Ensure route creation is successful
         route_id = create_response.json()["id"]
 
         # Luego la eliminamos
-        response = await client.delete(f"/api/routes/{route_id}")
+        response = await client.delete(f"/routes/{route_id}")
         assert response.status_code == 200
         assert response.json()["message"] == f"Route {route_id} deleted"
