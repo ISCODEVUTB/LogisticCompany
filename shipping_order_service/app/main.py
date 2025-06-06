291d10924c78c015a3423e6aa91297e7977ec53f
davirg12 <riveran@utb.edu.co>
2025-06-05 19:37:02 -0500

    Update main.py

--- shipping_order_service/app/main.py
+++ shipping_order_service/app/main.py
@@ -1,23 +1,9 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from app.api.routes import router as shipping_order_router
 
-from .api.routes import router as shipping_order_router
-
-app = FastAPI(title="Shipping Order Microservice", version="1.0.0")
-
-# Configuración de CORS
-origins = [
-    "http://localhost:8080",
-    "http://localhost:8000",
-    # "https://tu-front-end.com",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+app = FastAPI(
+    title="Shipping Order Microservice",
+    version="1.0.0"
 )
 
-app.include_router(shipping_order_router, prefix="/api", tags=["Orders"])
+app.include_router(shipping_order_router, prefix="", tags=["Orders"])
