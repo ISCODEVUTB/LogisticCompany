9ce516b9622c60ff6caf8acdef50c9c26f50d9a8
Amaury06-19 <95543963+Amaury06-19@users.noreply.github.com>
2025-06-06 15:41:04 +0000

    posible arreglo cambio "400" por "404"

--- shipping_order_service/test/negative_integration_tests/test_cancel_order_not_existent_id.py
+++ shipping_order_service/test/negative_integration_tests/test_cancel_order_not_existent_id.py
@@ -7,6 +7,6 @@ async def test_cancel_order_not_found():
     transport = ASGITransport(app=app)
     async with AsyncClient(transport=transport, base_url="https://test") as client:
         response = await client.post("/orders/nonexistent-id/cancel")
-        assert response.status_code == 400
+        assert response.status_code == 404
         assert response.json()["detail"] == "Unable to cancel the order"
 
