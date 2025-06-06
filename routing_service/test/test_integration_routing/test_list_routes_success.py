0c3ec9476d95b10555857565071fc1d6d1316e92
davirg12 <riveran@utb.edu.co>
2025-06-05 19:53:28 -0500

    Update test_list_routes_success.py

--- routing_service/test/test_integration_routing/test_list_routes_success.py
+++ routing_service/test/test_integration_routing/test_list_routes_success.py
@@ -1,14 +1,12 @@
 import pytest
-from httpx import ASGITransport, AsyncClient
-
-from routing_service.app.main import app
-
+from httpx import AsyncClient, ASGITransport
+from app.main import app
 
 @pytest.mark.asyncio
 async def test_list_routes_success():
     transport = ASGITransport(app=app)
     async with AsyncClient(transport=transport, base_url="https://test") as client:
-        response = await client.get("/api/routes/")
+        response = await client.get("/routes/")
 
         assert response.status_code == 200
         data = response.json()
