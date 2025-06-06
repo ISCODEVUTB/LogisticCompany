817e65051a9edc668b04dc093e9c56a881dc90a2
davirg12 <riveran@utb.edu.co>
2025-06-05 20:15:18 -0500

    Update build.gradle.kts

--- front-end/logistic_app/android/build.gradle.kts
+++ front-end/logistic_app/android/build.gradle.kts
@@ -1,9 +1,3 @@
-plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
-    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
-}
-
 allprojects {
     repositories {
         google()
@@ -18,13 +12,10 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
-// subprojects {
-//     project.evaluationDependsOn(":app")
-// } // This line is removed as per instructions
+subprojects {
+    project.evaluationDependsOn(":app")
+}
 
 tasks.register<Delete>("clean") {
-    description = "Elimina el directorio de construcción de todos los proyectos"
-    group = "build"
-
     delete(rootProject.layout.buildDirectory)
 }
