09446125ba4f98328e58480713cbe960da438743
davirg12 <riveran@utb.edu.co>
2025-06-05 19:38:00 -0500

    Update test_cancel_order_not_existent_id.py

--- shipping_order_service/test/negative_integration_tests/test_cancel_order_not_existent_id.py
+++ shipping_order_service/test/negative_integration_tests/test_cancel_order_not_existent_id.py
@@ -1,14 +1,12 @@
 import pytest
 from httpx import AsyncClient
+from app.main import app
 from httpx._transports.asgi import ASGITransport
-
-from shipping_order_service.app.main import app
-
-
 @pytest.mark.asyncio
 async def test_cancel_order_not_found():
     transport = ASGITransport(app=app)
     async with AsyncClient(transport=transport, base_url="https://test") as client:
-        response = await client.post("/api/orders/nonexistent-id/cancel")
+        response = await client.post("/orders/nonexistent-id/cancel")
         assert response.status_code == 400
         assert response.json()["detail"] == "Unable to cancel the order"
+
