cc4d81fe33729c549b4a24133c1a875bb49edb55
davirg12 <riveran@utb.edu.co>
2025-06-05 19:38:15 -0500

    Update test_crawling_not_existent_code.py

--- shipping_order_service/test/negative_integration_tests/test_crawling_not_existent_code.py
+++ shipping_order_service/test/negative_integration_tests/test_crawling_not_existent_code.py
@@ -1,14 +1,12 @@
 import pytest
 from httpx import AsyncClient
+from app.main import app
 from httpx._transports.asgi import ASGITransport
-
-from shipping_order_service.app.main import app
-
-
 @pytest.mark.asyncio
 async def test_track_order_not_found():
     transport = ASGITransport(app=app)
     async with AsyncClient(transport=transport, base_url="https://test") as client:
-        response = await client.get("/api/orders/track/invalid-tracking-code")
+        response = await client.get("/orders/track/invalid-tracking-code")
         assert response.status_code == 404
         assert response.json()["detail"] == "Tracking code not found"
+
