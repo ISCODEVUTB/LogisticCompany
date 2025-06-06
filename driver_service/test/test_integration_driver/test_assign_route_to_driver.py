2f6a93a5ab5f1a8c9a405aaa1ae33e42e7ebc095
davirg12 <riveran@utb.edu.co>
2025-06-05 20:01:10 -0500

    Update test_assign_route_to_driver.py

--- driver_service/test/test_integration_driver/test_assign_route_to_driver.py
+++ driver_service/test/test_integration_driver/test_assign_route_to_driver.py
@@ -1,9 +1,7 @@
 import pytest
 from httpx import AsyncClient
 from httpx._transports.asgi import ASGITransport
-
 from driver_service.app.main import app
-
 
 @pytest.mark.asyncio
 async def test_assign_route_success():
@@ -14,17 +12,12 @@ async def test_assign_route_success():
             "name": "Laura Rodríguez",
             "license_id": "ROUTE001",
             "phone": "3101234567",
-            "email": "laura@example.com",
+            "email": "laura@example.com"
         }
-        response_create = await client.post("/api/drivers/", json=payload)
+        response_create = await client.post("/drivers/", json=payload)
         driver_id = response_create.json()["driver_id"]
 
         # Asignar ruta
-        response_assign = await client.patch(
-            f"/api/drivers/{driver_id}/assign-route?route_id=R123"
-        )
+        response_assign = await client.patch(f"/drivers/{driver_id}/assign-route?route_id=R123")
         assert response_assign.status_code == 200
-        assert (
-            response_assign.json()["message"]
-            == f"Route R123 assigned to driver {driver_id}"
-        )
+        assert response_assign.json()["message"] == f"Route R123 assigned to driver {driver_id}"
