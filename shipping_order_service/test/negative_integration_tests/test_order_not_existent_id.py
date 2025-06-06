f4eaa3d7d429374fbb5f85f557a2ef6d1e5bf24a
davirg12 <riveran@utb.edu.co>
2025-06-05 19:38:31 -0500

    Update test_order_not_existent_id.py

--- shipping_order_service/test/negative_integration_tests/test_order_not_existent_id.py
+++ shipping_order_service/test/negative_integration_tests/test_order_not_existent_id.py
@@ -1,14 +1,12 @@
 import pytest
 from httpx import AsyncClient
+from app.main import app
 from httpx._transports.asgi import ASGITransport
-
-from shipping_order_service.app.main import app
-
-
 @pytest.mark.asyncio
 async def test_get_order_not_found():
     transport = ASGITransport(app=app)
     async with AsyncClient(transport=transport, base_url="https://test") as client:
-        response = await client.get("/api/orders/nonexistent-id")
+        response = await client.get("/orders/nonexistent-id")
         assert response.status_code == 404
         assert response.json()["detail"] == "Order not found"
+
