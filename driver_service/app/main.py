d27e6c56802260aaffa487dfab6d4b73a3b684f1
davirg12 <riveran@utb.edu.co>
2025-06-05 19:58:55 -0500

    Update main.py

--- driver_service/app/main.py
+++ driver_service/app/main.py
@@ -1,28 +1,11 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-
-from .api.routes import router as driver_router
+from driver_service.app.api.routes import router as driver_router
 
 app = FastAPI(
     title="Driver Microservice",
     description="Microservice for managing delivery drivers",
-    version="1.0.0",
-)
-
-# Configuración de CORS
-origins = [
-    "http://localhost:8080",  # Flutter web local
-    "http://localhost:8000",  # Otro posible puerto del frontend
-    # "https://tu-front-end.com",  # Producción, descomenta si aplica
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    version="1.0.0"
 )
 
 # Registrar las rutas bajo el prefijo /drivers
-app.include_router(driver_router, prefix="/api/drivers", tags=["Drivers"])
+app.include_router(driver_router, prefix="/drivers", tags=["Drivers"])
