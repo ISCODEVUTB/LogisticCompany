66853a72811d1784739f52f81e15f9156f9429ca
davirg12 <riveran@utb.edu.co>
2025-06-05 19:28:07 -0500

    Update main.py

--- tracking_service/app/main.py
+++ tracking_service/app/main.py
@@ -1,6 +1,4 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-
 from app.api.routes import router as tracking_router
 
 app = FastAPI(
@@ -9,18 +7,4 @@ app = FastAPI(
     version="1.0.0",
 )
 
-origins = [
-    "http://localhost:8080",
-    "http://localhost:8000",
-    # "https://tu-front-end.com",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-app.include_router(tracking_router, prefix="/api/events", tags=["Tracking"])
+app.include_router(tracking_router, prefix="/tracking", tags=["Tracking"])
