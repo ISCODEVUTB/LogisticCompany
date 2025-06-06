41bc29fe622783b87715d39ef34bc5fd82920f89
davirg12 <riveran@utb.edu.co>
2025-06-05 19:41:24 -0500

    Update test_shipping_order.py

--- shipping_order_service/test/test_shipping_order.py
+++ shipping_order_service/test/test_shipping_order.py
@@ -1,29 +1,31 @@
 # test/test_shipping_order.py
+import pytest
 from datetime import datetime
 from unittest.mock import AsyncMock, patch
 
-import pytest
-
-from shipping_order_service.app.schemas.shipping_order_schema import \
-    ShippingOrderCreateDTO
-from shipping_order_service.app.services import shipping_order_service
+from app.schemas.shipping_order_schema import ShippingOrderCreateDTO
+from app.services import shipping_order_service
 
 
 @pytest.mark.asyncio
-@patch(
-    "shipping_order_service.app.services.shipping_order_service.send_tracking_event",
-    new_callable=AsyncMock,
-)
+@patch("app.services.shipping_order_service.send_tracking_event", new_callable=AsyncMock)
 async def test_create_shipping_order_success(mock_send_tracking_event):
     dto = ShippingOrderCreateDTO(
-        sender={"name": "Juan Pérez", "address": "Calle 123", "phone": "3123456789"},
+        sender={
+            "name": "Juan Pérez",
+            "address": "Calle 123",
+            "phone": "3123456789"
+        },
         receiver={
             "name": "María Gómez",
             "address": "Avenida 456",
-            "phone": "9876543210",
+            "phone": "9876543210"
         },
         pickup_date=datetime.now(),
-        package={"weight": 2.5, "dimensions": "30x10x20"},
+        package={
+            "weight": 2.5,
+            "dimensions": "30x10x20"
+        }
     )
 
     result = await shipping_order_service.create_shipping_order(dto)
