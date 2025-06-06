d15baf36cc6a92e68d20d2523368367812fb640c
davirg12 <riveran@utb.edu.co>
2025-06-05 19:27:24 -0500

    Update tracking_service.py

--- tracking_service/app/services/tracking_service.py
+++ tracking_service/app/services/tracking_service.py
@@ -1,17 +1,13 @@
-from ..models.tracking import TrackingEvent
-from ..repository.tracking_repo import (get_all_tracking_events,
-                                        get_tracking_events_by_code,
-                                        save_tracking_event)
-from ..schemas.tracking_schemas import (TrackingEventCreate, TrackingEventOut,
-                                        TrackingHistoryOut)
-
+from app.models.tracking import TrackingEvent
+from app.schemas.tracking_schemas import TrackingEventCreate, TrackingHistoryOut, TrackingEventOut
+from app.repository.tracking_repo import save_tracking_event, get_tracking_events_by_code
 
 class TrackingService:
     def add_tracking_event(self, data: TrackingEventCreate):
         event = TrackingEvent(
             tracking_code=data.tracking_code,
             status=data.status,
-            timestamp=data.timestamp,
+            timestamp=data.timestamp
         )
         save_tracking_event(event)
         return {"message": "Tracking event recorded"}
@@ -20,7 +16,3 @@ class TrackingService:
         events = get_tracking_events_by_code(tracking_code)
         history = [TrackingEventOut(**e.__dict__) for e in events]
         return TrackingHistoryOut(tracking_code=tracking_code, history=history)
-
-    def get_all_events(self):
-        events = get_all_tracking_events()
-        return [e.__dict__ for e in events]
