f024151969ccedbef65d6e79228118e72cc72bec
davirg12 <riveran@utb.edu.co>
2025-06-05 19:47:33 -0500

    Update main.py

--- routing_service/app/main.py
+++ routing_service/app/main.py
@@ -1,23 +1,5 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-
-from .api.routes import router as route_router
+from app.api.routes import router as route_router
 
 app = FastAPI(title="Routing Microservice")
-
-# Configuración de CORS
-origins = [
-    "http://localhost:8080",
-    "http://localhost:8000",
-    # "https://tu-front-end.com",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-app.include_router(route_router, prefix="/api")
+app.include_router(route_router)
